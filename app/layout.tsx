3523b9ee424123cac1a135d9897bc98d8c30f5b5
kaylomaliki <kaylomaliki@gmail.com>
2026-01-07 20:00:25 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.variable}>
+      <body className={inter.variable} suppressHydrationWarning>
         {children}
       </body>
     </html>
